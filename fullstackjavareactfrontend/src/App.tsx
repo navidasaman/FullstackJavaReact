0c528d35b1bfe81bad953d2646b88e7d3cc7b520
navidasaman <navidasaman@gmail.com>
2023-10-19 20:56:43 +0200

    Initialize the FrontEnd part of project React TypeScript TailwindCSS

---
+++ fullstackjavareactfrontend/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+  <div className="bg-gray-900 h-screen flex flex-col items-center justify-center">
+    <p className="font-bold text-teal-500">Built with Java, React, TypeScript, TailwindCSS</p>
+    <h1 className="text-2xl font-bold text-gray-500">Employee Management System</h1>
+    <hr/>
+  </div>
+  );
+};
+
+export default App;
