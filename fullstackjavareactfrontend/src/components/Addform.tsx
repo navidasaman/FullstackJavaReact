6296a340253db033a4eb501c0ac7d66b957eeedf
navidasaman <navidasaman@gmail.com>
2023-10-23 16:56:31 +0200

    Adds frontend option to edit employee

--- fullstackjavareactfrontend/src/components/Addform.tsx
+++ fullstackjavareactfrontend/src/components/Addform.tsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
-import {BsXLg} from 'react-icons/bs'
+import { BsXLg, BsFillPencilFill } from 'react-icons/bs'
+import '../index.css'
 
 interface Employee {
 	id: number;
@@ -13,13 +14,20 @@ interface Employee {
 
 const Addform: React.FC = () => {
 	const [name, setName] = useState('');
-	const [age, setAge] = useState(0);
+	const [age, setAge] = useState<number | undefined>(undefined);
 	const [occupation, setOccupation] = useState('');
 	const [department, setDepartment] = useState('');
-	const [salary, setSalary] = useState(0);
+	const [salary, setSalary] = useState<number | undefined>(undefined);
 	const [skills, setSkills] = useState('');
 	const [employees, setEmployees] = useState<Employee[]>([]);
 
+	const [editName, setEditName] = useState('');
+	const [editAge, setEditAge] = useState<number | undefined>(undefined);
+	const [editOccupation, setEditOccupation] = useState('');
+	const [editDepartment, setEditDepartment] = useState('');
+	const [editSalary, setEditSalary] = useState<number | undefined>(undefined);
+	const [editSkills, setEditSkills] = useState('');
+	const [showModal, setShowModal] = useState(false);
 
 	const handleSubmit = (e: any) => {
 		e.preventDefault()
@@ -35,32 +43,58 @@ const Addform: React.FC = () => {
 		}).catch((error) => {
 			console.error('Error adding employee:', error);
 		});
-	}
+	};
 
-	// Fetch data through HTTP GET request to the specified endpoint
 	useEffect(() => {
 		fetch(process.env.REACT_APP_API_URL_RETRIEVE_EMPLOYEES_ENDPOINT as string)
 			.then(res => res.json())
 			.then((result) => {
 				setEmployees(result);
 			})
-	}, [])
+	}, []);
 
 	const handleDelete = (id: number) => {
 		fetch(`${process.env.REACT_APP_API_URL_DELETE_EMPLOYEE_ENDPOINT}/${id}`, {
 			method: "DELETE",
 			headers: { "Content-Type": "application/json" },
-		  })
+		})
 			.then(() => {
-			  console.log("Employee successfully deleted!");
-			  // Update the employee list after successful deletion
-			  setEmployees(employees.filter((employee) => employee.id !== id));
+				console.log("Employee successfully deleted!");
+				// Update the employee list after successful deletion
+				setEmployees(employees.filter((employee) => employee.id !== id));
 			})
 			.catch((error) => {
-			  console.error("Error deleting employee:", error);
+				console.error("Error deleting employee:", error);
 			});
+	};
+
+	const handleEdit = (id: number) => {
+		const editedEmployee = {
+			name: editName,
+			age: editAge,
+			occupation: editOccupation,
+			department: editDepartment,
+			salary: editSalary,
+			skills: editSkills
 		};
-	
+		setShowModal(true);
+		fetch(`${process.env.REACT_APP_API_URL_EDIT_EMPLOYEE_ENDPOINT}/${id}`, {
+			method: "PUT",
+			headers: { "Content-Type": "application/json" },
+			body: JSON.stringify(editedEmployee)
+		})
+			.then(() => {
+				alert("After employee has been edited and save button been pressed, please press close. Remember, all fields must be filled correctly before saving.")
+			})
+			.catch((error) => {
+				console.error("Error updating employee:", error);
+			});
+	};
+
+	const handleCloseModal = () => {
+		setShowModal(false);
+		window.location.reload();
+	};
 
 	return (
 		<div className="w-screen h-4/5 flex flex-row items-center justify-center">
@@ -149,7 +183,7 @@ const Addform: React.FC = () => {
 				<h1 className="text-2xl font-bold text-center text-gray-800 pb-10">Employee Registry</h1>
 				{employees.map((employee) => (
 					<div key={employee.id} className='bg-slate-100 rounded-md p-5 mb-2'>
-						<h2 className='font-bold flex items-center justify-between'>• {employee.name} • <BsXLg onClick={() => handleDelete(employee.id)} className="text-red-600 font-extrabold cursor-pointer" /> </h2>
+						<h2 className='font-bold flex items-center justify-between'>• {employee.name} • <BsFillPencilFill onClick={() => handleEdit(employee.id)} className="text-green-600 font-extrabold cursor-pointer " />  <BsXLg onClick={() => handleDelete(employee.id)} className="text-red-600 font-extrabold cursor-pointer" /> </h2>
 						<p>ID: {employee.id}</p>
 						<p>Age: {employee.age}</p>
 						<p>Occupation: {employee.occupation}</p>
@@ -159,6 +193,57 @@ const Addform: React.FC = () => {
 					</div>
 				))}
 			</div>
+			{showModal && employees.map((employee) => (
+				<div className="fixed w-full top-0 left-0 h-full  bg-black bg-opacity-50 flex justify-center items-center z-50">
+					<div className="bg-gradient-to-r from-sky-500 to-indigo-500 shadow-lg rounded border border-gray-200 border-opacity-20 p-3 min-w-[calc(100%-1100px)] min-h-[fit-content]">
+						<h2 className="“mt-0 lowercase tracking-widest text-3xl font-thin text-white opacity-75 select-none mb-2">Edit Employee</h2>
+						<input
+							type="text"
+							value={editName}
+							onChange={(e) => setEditName(e.target.value)}
+							placeholder="Name"
+							className='flex w-full h-9 p-1 m-auto bg-white border-b-0 border-l-0 border-r-0 mb-2'
+						/>
+						<input
+							type="number"
+							value={editAge}
+							onChange={(e) => setEditAge(parseInt(e.target.value))}
+							placeholder="Age"
+							className='flex w-full h-9 p-1 m-auto bg-white border-b-0 border-l-0 border-r-0  mb-2'
+						/>
+						<input
+							type="text"
+							value={editOccupation}
+							onChange={(e) => setEditOccupation(e.target.value)}
+							placeholder="Occupation"
+							className='flex w-full h-9 p-1 m-auto bg-white border-b-0 border-l-0 border-r-0  mb-2'
+						/>
+						<input
+							type="text"
+							value={editDepartment}
+							onChange={(e) => setEditDepartment(e.target.value)}
+							placeholder="Department"
+							className='flex w-full h-9 p-1 m-auto bg-white border-b-0 border-l-0 border-r-0  mb-2'
+						/>
+						<input
+							type="number"
+							value={editSalary}
+							onChange={(e) => setEditSalary(parseInt(e.target.value))}
+							placeholder="Salary"
+							className='flex w-full h-9 p-1 m-auto bg-white border-b-0 border-l-0 border-r-0 mb-2'
+						/>
+						<textarea
+							value={editSkills}
+							onChange={(e) => setEditSkills(e.target.value)}
+							placeholder="Skills"
+							className='overflow-y-scroll h-48 w-full'
+						>
+						</textarea>
+						<button onClick={() => handleEdit(employee.id)} className='mr-2.5 h-7 w-full bg-gray-800 text-white border-0 mt-1 tracking-widest lowercase font-thin cursor-pointer'>Save</button>
+						<button onClick={handleCloseModal} className='mr-2.5 h-7 w-full bg-gray-800 text-white border-0 mt-1 tracking-widest lowercase font-thin cursor-pointer'>Close</button>
+					</div>
+				</div>
+			))}
 		</div>
 	);
 };
